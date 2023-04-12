46bd79aef6a8e140ac19bcbab40305b422962284
EndaMcCabe <33401576+EndaMcCabe@users.noreply.github.com>
2023-04-12 17:50:01 +0100

    Add a deck class with shuffle and deal one functions to it

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 '''
 These are the values that will be associated to the cards in the game.
 '''
@@ -8,12 +10,33 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
             'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
 
 class Card:
+    
     def __init__ (self, rank, suit):
         self.rank = rank
         self.suit = suit
         self.value = values[rank]
 
-    def __str__(self) -> str:
-        return 'This is a card'
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
 
-card = Card('Pikachu', 'Yellow')
+class Deck:
+
+    def __init__(self):
+
+        '''
+        This creates the deck by looping through the suits and ranks tuples and creating 52 unique cards which will be our Deck
+        '''
+
+        self.all_cards = []
+        for suit in suits:
+            for rank in ranks:
+                self.all_cards.append(Card(rank, suit))
+
+        #This will shuffle the deck
+        def shuffle(self):
+            random.shuffle(self.all_cards)
+        
+        #This will remove one Card from the deck acting like a dealer dealing one card
+        def deal_one(self):
+            return self.all_cards.pop()
+
