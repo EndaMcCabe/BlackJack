02c3ee401da07d906f91cc05d5800583541a3de3
EndaMcCabe <33401576+EndaMcCabe@users.noreply.github.com>
2023-04-12 17:41:32 +0100

    Add the possible values for the cards and made the card class

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+These are the values that will be associated to the cards in the game.
+'''
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
+            'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+
+class Card:
+    def __init__ (self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        self.value = values[rank]
+
+    def __str__(self) -> str:
+        return 'This is a card'
+
+card = Card('Pikachu', 'Yellow')
